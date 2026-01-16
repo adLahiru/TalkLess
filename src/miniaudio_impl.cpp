d1aae41acd4228a3611f26f4f256cd5758586c5c
Thilokya Angeesa <thilokyaangeesa@gmail.com>
2026-01-16 23:19:17 +0530

    Ogg files are playing

--- src/miniaudio_impl.cpp
+++ src/miniaudio_impl.cpp
@@ -1,3 +1,16 @@
 // miniaudio_impl.cpp
+// This file implements miniaudio with support for OGG/Vorbis files
+
+// Enable stb_vorbis for OGG decoding support in miniaudio
+// stb_vorbis must be included BEFORE miniaudio.h
+#define STB_VORBIS_HEADER_ONLY
+#include "stb_vorbis.c"  // This provides the declarations
+
+// Now include miniaudio with the implementation
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
+
+// Include the stb_vorbis implementation 
+// (must be after miniaudio since it uses the declarations above)
+#undef STB_VORBIS_HEADER_ONLY
+#include "stb_vorbis.c"
